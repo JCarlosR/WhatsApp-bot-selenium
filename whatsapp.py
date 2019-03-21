2659c506faaa5459a1b0a99b8f541bea5c625040
Juan Ramos <juancagb.17@gmail.com>
2019-03-21 01:09:36 -0500

    Fix contact search and improve debug messages

--- whatsapp.py
+++ whatsapp.py
@@ -1,8 +1,7 @@
-# Note: For proper working of this Script Good and Uninterepted Internet Connection is Required
 # Keep all contacts unique
 # Can save contact with their phone Number
 
-# Import required packages
+# Required packages
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -12,7 +11,7 @@ import datetime
 import time
 import openpyxl as excel
 
-# function to read contacts from a text file
+# Read contacts from a xlsx file
 def readContacts(fileName):
     lst = []
     file = excel.load_workbook(fileName)
@@ -32,7 +31,9 @@ targets = readContacts("contacts.xlsx")
 print(targets)
 
 # Driver to open a browser
-driver = webdriver.Firefox()
+options = webdriver.ChromeOptions()
+options.add_argument('user-data-dir=D:\Python\WhatsAppBot\DataChrome')
+driver = webdriver.Chrome(options=options)
 
 #link to open a site
 driver.get("https://web.whatsapp.com/")
@@ -44,22 +45,26 @@ wait = WebDriverWait(driver, 10)
 wait5 = WebDriverWait(driver, 5)
 input("Scan the QR code and then press Enter")
 
-# Message to send list
-# 1st Parameter: Hours in 0-23
-# 2nd Parameter: Minutes
-# 3rd Parameter: Seconds (Keep it Zero)
-# 4th Parameter: Message to send at a particular time
-# Put '\n' at the end of the message, it is identified as Enter Key
-# Else uncomment Keys.Enter in the last step if you dont want to use '\n'
-# Keep a nice gap between successive messages
-# Use Keys.SHIFT + Keys.ENTER to give a new line effect in your Message
+# Messages to send (at a particular time)
+# 1st parameter: Hour (0-23)
+# 2nd parameter: Minutes
+# 3rd parameter: Seconds (keep it zero)
+# 4th parameter: Text message to send 
+
+# You can use '\n' in the message, it is identified as Enter Key.
+# Keep a nice gap between successive messages.
+# Keys.SHIFT + Keys.ENTER give a new line effect in your message.
 msgToSend = [
-                [12, 32, 0, "Hello! This is test Msg. Please Ignore." + Keys.SHIFT + Keys.ENTER + "http://bit.ly/mogjm05"]
-            ]
+    (
+        22, 7, 0, 
+        # "This is an automated message sent from a bot (developed in Python). Please ignore." + Keys.SHIFT + Keys.ENTER + "https://programacionymas.com"
+        "This is an automated message sent from a bot (developed in Python). Please ignore."
+    )
+]
 
 # Count variable to identify the number of messages to be sent
 count = 0
-while count<len(msgToSend):
+while count < len(msgToSend):
 
     # Identify time
     curTime = datetime.datetime.now()
@@ -69,7 +74,7 @@ while count<len(msgToSend):
 
     # if time matches then move further
     if msgToSend[count][0]==curHour and msgToSend[count][1]==curMin and msgToSend[count][2]==curSec:
-        # utility variables to tract count of success and fails
+        # variables to track count of success and fails
         success = 0
         sNo = 1
         failList = []
@@ -80,58 +85,71 @@ while count<len(msgToSend):
             sNo+=1
             try:
                 # Select the target
-                x_arg = '//span[contains(@title,' + target + ')]'
+                x_arg = '//span[contains(@title,' + target + ') and contains(@class, "_1wjpf")]'
                 try:
                     wait5.until(EC.presence_of_element_located((
                         By.XPATH, x_arg
                     )))
                 except:
                     # If contact not found, then search for it
-                    searBoxPath = '//*[@id="input-chatlist-search"]'
                     wait5.until(EC.presence_of_element_located((
-                        By.ID, "input-chatlist-search"
+                        By.CSS_SELECTOR, '.jN-F5.copyable-text.selectable-text'
                     )))
-                    inputSearchBox = driver.find_element_by_id("input-chatlist-search")
+
+                    inputSearchBox = driver.find_element_by_css_selector('.jN-F5.copyable-text.selectable-text')
+                    # print('class name found (input search box)', inputSearchBox)
                     time.sleep(0.5)
+
                     # click the search button
-                    driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div[2]/div/button').click()
+                    searchButton = driver.find_element_by_xpath('/html/body/div/div/div/div[3]/div/div/div/button')
+                    # print('xpath found (search button)', inputSearchBox)
+                    searchButton.click()
                     time.sleep(1)
+
                     inputSearchBox.clear()
                     inputSearchBox.send_keys(target[1:len(target) - 1])
-                    print('Target Searched')
+                    print('Target searched')
                     # Increase the time if searching a contact is taking a long time
-                    time.sleep(4)
+                    time.sleep(3)
 
                 # Select the target
                 driver.find_element_by_xpath(x_arg).click()
-                print("Target Successfully Selected")
+                print('Target successfully selected: ', x_arg)
                 time.sleep(2)
 
                 # Select the Input Box
                 inp_xpath = "//div[@contenteditable='true']"
                 input_box = wait.until(EC.presence_of_element_located((
-                    By.XPATH, inp_xpath)))
+                    By.XPATH, inp_xpath
+                )))
                 time.sleep(1)
 
                 # Send message
                 # taeget is your target Name and msgToSend is you message
-                input_box.send_keys("Hello, " + target + "."+ Keys.SHIFT + Keys.ENTER + msgToSend[count][3] + Keys.SPACE) # + Keys.ENTER (Uncomment it if your msg doesnt contain '\n')
-                # Link Preview Time, Reduce this time, if internet connection is Good
-                time.sleep(10)
+                input_box.send_keys(f'Hello {target}!')
+                input_box.send_keys(Keys.SHIFT + Keys.ENTER)
+                input_box.send_keys(msgToSend[count][3] + Keys.SPACE)
+
+                # Wait for link preview (reduce this time if internet connection is good)
+                # time.sleep(3)
+
                 input_box.send_keys(Keys.ENTER)
-                print("Successfully Send Message to : "+ target + '\n')
-                success+=1
+                print("Successfully sent message to : "+ target + '\n')
+
+                success += 1
                 time.sleep(0.5)
 
-            except:
-                # If target Not found Add it to the failed List
-                print("Cannot find Target: " + target)
+            except Exception as e:
+                # If target not found, add it to the failed List
+                print("Could not find target: " + target)
+                print(e)
                 failList.append(target)
                 pass
 
-        print("\nSuccessfully Sent to: ", success)
-        print("Failed to Sent to: ", len(failList))
+        print("\nSuccessfully sent to: ", success)
+        print("Failed messages: ", len(failList))
         print(failList)
         print('\n\n')
-        count+=1
+        count += 1
+
 driver.quit()
